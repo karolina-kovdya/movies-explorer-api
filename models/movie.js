0870d4b32b002af88c918380c2f13fa7f2615728
Karolina Kovdya <karolina.kovdya-onikienko@yandex.ru>
2023-04-11 22:05:54 +0300

    добавляет модель фильма

---
+++ models/movie.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    required: true,
+    type: String,
+  },
+  director: {
+    required: true,
+    type: String,
+  },
+  duration: {
+    required: true,
+    type: Number,
+  },
+  year: {
+    required: true,
+    type: String,
+  },
+  description: {
+    required: true,
+    type: String,
+  },
+  image: {
+    required: true,
+    type: String,
+    validate: {
+      validator(v) {
+        return validator.isUrl(v);
+      },
+      message: 'Некорректный URL',
+    },
+  },
+  trailerLink: {
+    required: true,
+    type: String,
+    validate: {
+      validator(v) {
+        return validator.isUrl(v);
+      },
+      message: 'Некорректный URL',
+    },
+  },
+  thumbnail: {
+    required: true,
+    type: String,
+    validate: {
+      validator(v) {
+        return validator.isUrl(v);
+      },
+      message: 'Некорректный URL',
+    },
+  },
+  oener: {
+    required: true,
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+  },
+  movieId: {
+    required: true,
+    tupe: Number,
+  },
+  nameRU: {
+    required: true,
+    type: String,
+  },
+  nameEN: {
+    required: true,
+    type: String,
+  },
+});
+
+const Movie = mongoose.model('user', movieSchema);
+
+module.exports = Movie;
